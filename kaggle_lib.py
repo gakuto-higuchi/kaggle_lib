5f48fa9ef3b4c75c976196d5f1bb4c0afd8302b8
gakuto-higuchi <82492341+gakuto-higuchi@users.noreply.github.com>
2023-09-27 15:55:44 +0900

    Create kaggle_lib.py

---
+++ kaggle_lib.py
@@ -0,0 +1,35 @@
+// GPU使用
+if torch.cuda.is_available():
+  model.to("cuda")
+
+// GPUメモリの使用量をモニタする関数
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+def print_summary(result): 
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
+
+result = trainer.train() // モデルを学習
+print_summray(result)
+
+// シード値をあらかじめ設定
+def seed_everything(seed: int):
+    import random, os
+    import numpy as np
+    import torch
+    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    
+seed_everything(seed=42)
+
